1089adb14d5b4c8a48eb0a58ac06b5edea44ad0d
Oleg Kean <oleg.kean@gmail.com>
2015-11-21 22:42:47 +0200

    Build batches

---
+++ 3rd-party/build.debug.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set MSBUILD_PATH="C:\Program Files (x86)\MSBuild\12.0\Bin\"
+
+if exist %MSBUILD_PATH% goto build
+
+@echo Not able to find MSBuild executable. Please check your Microsoft Build Tool installation.
+goto end
+
+:build
+cd %MSBUILD_PATH%
+
+set PROJECT_PATH=%~dp0
+msbuild.exe %PROJECT_PATH%3rd-party.sln /t:Rebuild /p:configuration=debug
+
+:end
+pause
